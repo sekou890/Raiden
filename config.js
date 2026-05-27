72aef5c752bbfa213bc496f718f5563e835cc0b5
sekou890 <remuru159@gmail.com>
2026-05-28 03:34:16 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 
-module.exports = {
+  module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["962994407651553302", ""], 
-  mongodbUri : "mongodb + srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&&w=majority",
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&&w=majority",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
   setupFilePath: './commands/setup.json',
